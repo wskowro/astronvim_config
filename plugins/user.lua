51c2c134fad8bdebd133b7e2420338041dedf04b
wskowro <wskowro@gmail.com>
2023-04-24 21:20:59 -0400

    Made first changes to neovim config files from youtube video

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,5 @@
+-- TODO: Test
+
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,4 +11,21 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end,
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoQuickFix<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
 }
